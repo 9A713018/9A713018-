5407e4a786b4847c5b7a21ccb423295ff46c959b
9A713018 <72920081+9A713018@users.noreply.github.com>
2020-11-19 23:39:16 +0800

    Add files via upload

---
+++ 9A713018期中/9A713018/9A713018/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿
+namespace _9A713018
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.tbInput = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(222, 172);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(154, 65);
+            this.btnGo.TabIndex = 0;
+            this.btnGo.Text = "請輸入數字";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // tbInput
+            // 
+            this.tbInput.Location = new System.Drawing.Point(200, 78);
+            this.tbInput.Name = "tbInput";
+            this.tbInput.Size = new System.Drawing.Size(199, 64);
+            this.tbInput.TabIndex = 1;
+            this.tbInput.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 360);
+            this.Controls.Add(this.tbInput);
+            this.Controls.Add(this.btnGo);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.RichTextBox tbInput;
+    }
+}
+
---
+++ 9A713018期中/9A713018/9A713018/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _9A713018
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+            
+        {
+            try
+            {
+                
+                string input_str = tbInput.Text;
+                
+                int input_int = int.Parse(input_str);
+
+                
+
+                
+                MessageBox.Show($"{input_int}", "Num", MessageBoxButtons.OK);
+            }
+            catch (Exception)
+            {
+                
+                MessageBox.Show($"請打數字!!!", "警告", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                
+            }
+        }
+    }
+}
